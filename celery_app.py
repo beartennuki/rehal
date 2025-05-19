41762f7ebcff7fd634b5f617f4b4be3e21b19b65
qunox-01 <qunox@xanary-3.local>
2025-05-19 09:38:11 +0800

    change to use redis

--- celery_app.py
+++ celery_app.py
@@ -1,13 +1,15 @@
-# celery_app.py
 from celery import Celery
 
 celery_app = Celery(
     'rehal',
-    broker='amqp://guest:guest@localhost:5672//',
-    backend='mongodb://localhost:27017/celery_beckend_db'
+    broker='redis://localhost:6379/0',
+    backend='redis://localhost:6379/1'
 )
 
 celery_app.conf.update(
     result_expires=3600,  # Results expire after 1 hour
-) # this does not work, have to set manually TTL in mongosh
-import src.job.submit
+    task_serializer='json',
+    result_serializer='json',
+    accept_content=['json'],
+    broker_transport_options={'visibility_timeout': 3600},  # Optional: Adjust as needed
+)
